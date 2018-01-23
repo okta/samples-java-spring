9249e0fba86c2a1f0798c911d7d27a69adfdada9
Brian Demers <bdemers@apache.org>
2018-01-23 15:08:36 -0500

    Cleanup whitespace in examples
    I'm not sure how this snuck in, but sorry for the tabs!

--- basic/src/main/java/com/okta/spring/example/CodeFlowExampleApplication.java
+++ basic/src/main/java/com/okta/spring/example/CodeFlowExampleApplication.java
@@ -23,64 +23,64 @@ import java.util.Collections;
 @SpringBootApplication
 public class CodeFlowExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodeFlowExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodeFlowExampleApplication.class, args);
+    }
 
-	/**
-	 * Enable the use of {@link PreAuthorize} annotation and OAuth expressions like {code}#oauth2.hasScope('email'){code}.
-	 */
-	@EnableGlobalMethodSecurity(prePostEnabled = true)
-	protected static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
-		@Override
-		protected MethodSecurityExpressionHandler createExpressionHandler() {
-			return new OAuth2MethodSecurityExpressionHandler();
-		}
-	}
+    /**
+     * Enable the use of {@link PreAuthorize} annotation and OAuth expressions like {code}#oauth2.hasScope('email'){code}.
+     */
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    protected static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            return new OAuth2MethodSecurityExpressionHandler();
+        }
+    }
 
-	/**
-	 * The default Spring logout behavior redirects a user back to {code}/login?logout{code}, so you will likely want
-	 * to change that.  The easiest way to do this is by both extending from {@link OAuth2SsoDefaultConfiguration} and
-	 * annotating your implementation with {@link EnableOAuth2Sso}.
-	 */
+    /**
+     * The default Spring logout behavior redirects a user back to {code}/login?logout{code}, so you will likely want
+     * to change that.  The easiest way to do this is by both extending from {@link OAuth2SsoDefaultConfiguration} and
+     * annotating your implementation with {@link EnableOAuth2Sso}.
+     */
     @Configuration
     @EnableOAuth2Sso
-	static class ExampleSecurityConfigurerAdapter extends OAuth2SsoDefaultConfiguration {
+    static class ExampleSecurityConfigurerAdapter extends OAuth2SsoDefaultConfiguration {
 
         public ExampleSecurityConfigurerAdapter(ApplicationContext applicationContext, OAuth2SsoProperties sso) {
             super(applicationContext, sso);
         }
 
         @Override
-		protected void configure(HttpSecurity http) throws Exception {
+        protected void configure(HttpSecurity http) throws Exception {
 
-        	// In this example we allow anonymous access to the root index page
-			// this MUST be configured before calling super.configure
+            // In this example we allow anonymous access to the root index page
+            // this MUST be configured before calling super.configure
             http.authorizeRequests().antMatchers("/").permitAll();
 
             // calling super.configure locks everything else down
-			super.configure(http);
-        	// after calling super, you can change the logout success url
-			http.logout().logoutSuccessUrl("/");
-		}
-	}
+            super.configure(http);
+            // after calling super, you can change the logout success url
+            http.logout().logoutSuccessUrl("/");
+        }
+    }
 
-	/**
-	 * This example controller has endpoints for displaying the user profile info on {code}/{code} and "you have been
-	 * logged out page" on {code}/post-logout{code}.
-	 */
-	@Controller
-	public class ExampleController {
+    /**
+     * This example controller has endpoints for displaying the user profile info on {code}/{code} and "you have been
+     * logged out page" on {code}/post-logout{code}.
+     */
+    @Controller
+    public class ExampleController {
 
-		@GetMapping("/")
-		public String home() {
-			return "home";
-		}
+        @GetMapping("/")
+        public String home() {
+            return "home";
+        }
 
-		@GetMapping("/profile")
-		@PreAuthorize("#oauth2.hasScope('profile')")
-		public ModelAndView userDetails(OAuth2Authentication authentication) {
-			return new ModelAndView("userProfile" , Collections.singletonMap("details", authentication.getUserAuthentication().getDetails()));
-		}
-	}
+        @GetMapping("/profile")
+        @PreAuthorize("#oauth2.hasScope('profile')")
+        public ModelAndView userDetails(OAuth2Authentication authentication) {
+            return new ModelAndView("userProfile" , Collections.singletonMap("details", authentication.getUserAuthentication().getDetails()));
+        }
+    }
 }
