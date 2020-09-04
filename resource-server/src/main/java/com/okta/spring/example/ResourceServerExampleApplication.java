96f39182e9117d0210a62460e7205efeb9bd0588
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-09-04 11:10:15 -0700

    Update to spring boot v2.3.3.RELEASE; update resource server controller to support both jwt & opaque token validation

--- resource-server/src/main/java/com/okta/spring/example/ResourceServerExampleApplication.java
+++ resource-server/src/main/java/com/okta/spring/example/ResourceServerExampleApplication.java
@@ -8,7 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.AbstractOAuth2TokenAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +32,7 @@ public class ResourceServerExampleApplication {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests()
-                .anyRequest().authenticated()
-            .and()
-                .oauth2ResourceServer().jwt();
+                .anyRequest().authenticated();
 
             // process CORS annotations
             http.cors();
@@ -50,7 +48,7 @@ public class ResourceServerExampleApplication {
 
         @GetMapping("/api/userProfile")
         @PreAuthorize("hasAuthority('SCOPE_profile')")
-        public Map<String, Object> getUserDetails(JwtAuthenticationToken authentication) {
+        public Map<String, Object> getUserDetails(AbstractOAuth2TokenAuthenticationToken authentication) {
             return authentication.getTokenAttributes();
         }
 
