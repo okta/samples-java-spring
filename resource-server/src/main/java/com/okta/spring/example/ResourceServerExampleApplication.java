d9e93c8a040c83b259567afe8b295365b19e334c
Sergii Shamrai <71881713+sergiishamrai-okta@users.noreply.github.com>
2020-12-16 19:32:04 +0200

    Working code for JWT and Opaque tokens

--- resource-server/src/main/java/com/okta/spring/example/ResourceServerExampleApplication.java
+++ resource-server/src/main/java/com/okta/spring/example/ResourceServerExampleApplication.java
@@ -8,7 +8,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
+import org.springframework.security.oauth2.server.resource.authentication.AbstractOAuth2TokenAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,7 +51,7 @@ public class ResourceServerExampleApplication {
 
         @GetMapping("/api/userProfile")
         @PreAuthorize("hasAuthority('SCOPE_profile')")
-        public Map<String, Object> getUserDetails(JwtAuthenticationToken authentication) {
+        public <A extends AbstractOAuth2TokenAuthenticationToken<AbstractOAuth2Token>> Map<String, Object> getUserDetails(A authentication) {
             return authentication.getTokenAttributes();
         }
 
